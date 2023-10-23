1ce7457986544ed21f578dd91fbc7b8686f2e9aa
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-10-23 14:35:23 +0300

    Update Madeatior I

---
+++ CQRS_Library/CQRS/Handelrs/GetItemListHandelar.cs
@@ -0,0 +1,20 @@
+﻿using CQRS_Library.CQRS.Commands;
+using CQRS_Library.CQRS.Queries;
+using CQRS_Library.Data.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS_Library.CQRS.Handelrs
+{
+    public class GetItemListHandelar : IRequestHandler<GetAllQueries, List<Items>>
+    {
+        public Task<List<Items>> Handle(GetAllQueries request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
