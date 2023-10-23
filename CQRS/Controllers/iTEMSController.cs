50f667755d12fa12acedd7149fcb71de4dd23cb0
Moamen ASHRAF <moamen.ashraf@intalio.com>
2023-10-23 15:04:55 +0300

    UpdateMediatorController

--- CQRS/Controllers/iTEMSController.cs
+++ CQRS/Controllers/iTEMSController.cs
@@ -1,5 +1,9 @@
-﻿using CQRS_Library.Data.Models;
+﻿using CQRS_Library.CQRS.Commands;
+using CQRS_Library.CQRS.Handelrs;
+using CQRS_Library.CQRS.Queries;
+using CQRS_Library.Data.Models;
 using CQRS_Library.Repos;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,26 +14,36 @@ namespace CQRS.Controllers
     public class ItemsController : ControllerBase
     {
         private readonly IitemRepository iitemRepository;
+        private readonly IMediator mediator;
 
-        public ItemsController(IitemRepository iitemRepository)
+        public ItemsController(IitemRepository iitemRepository , IMediator mediator) 
         {
             this.iitemRepository = iitemRepository;
+            this.mediator = mediator;
         }
 
         [HttpGet]
 
         public async Task<IActionResult> GetItems()
         {
-            return Ok(iitemRepository.GetItems());
+            //return Ok(iitemRepository.GetItems());
+
+            var result = await mediator.Send(new GetAllQueries());
+
+            return Ok(result);
         }
 
         [HttpPost]
 
         public async Task<IActionResult> InsertItem(Items item)
         {
-            iitemRepository.InsertItem(item);
+            //iitemRepository.InsertItem(item);
 
-            return Ok(item);    
+            //return Ok(item);    
+
+            var result = await mediator.Send(new InsertCommand(item));
+
+            return Ok(result);
         }
     }
 }
